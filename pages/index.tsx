dcf060b0e2d41c6557416e2e3e5122271683ef57
vuquangit <vuquangit@gmail.com>
2020-09-27 01:42:25 +0700

    Modify homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import { DefaultLayout } from 'layout'
 import { FC, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { get, isEqual } from 'lodash'
+import { /* useSelector, */ useDispatch } from 'react-redux'
+// import { get, isEqual } from 'lodash'
 
 import { updateProfile } from 'redux/Profile/Profile.action'
-import { IProfileData } from 'redux/Profile/actionTypes'
+// import { IProfileData } from 'redux/Profile/actionTypes'
 
 const HomePage: FC = () => {
-  const profileData: IProfileData = useSelector(
-    (state = {}) => get(state, 'profile.data', {}),
-    isEqual()
-  )
+  //   const profileData: IProfileData = useSelector(
+  //     (state = {}) => get(state, 'profile.data', {}),
+  //     isEqual()
+  //   )
 
   const dispatch = useDispatch()
 
@@ -21,7 +21,7 @@ const HomePage: FC = () => {
   return (
     <DefaultLayout>
       <div className="container">This is new HomePage</div>
-      <p>My name is {profileData.name}</p>
+      {/* <p>My name is {profileData.name}</p> */}
     </DefaultLayout>
   )
 }
