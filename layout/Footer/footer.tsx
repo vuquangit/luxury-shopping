7c0e8744166e4c845129564249fed8f6a807858a
vuquangit <vuquangit@gmail.com>
2020-10-15 15:55:35 +0700

    Create footer page

--- layout/Footer/footer.tsx
+++ layout/Footer/footer.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react'
+import FooterLogo from './FooterLogo'
+import FooterNavigation from './FooterNavigation'
 
 const Footer: FC = () => {
-  return <footer>This is footer</footer>
+  return (
+    <footer role="contentinfo" className="default-layout__footer">
+      <FooterLogo />
+      <FooterNavigation />
+    </footer>
+  )
 }
 
 export default Footer
